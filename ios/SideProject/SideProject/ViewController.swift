0f991c839799321ed6d4795451bdba11f1bd316e
Gordon Krull <gordonekru@gmail.com>
2017-09-28 17:10:49 +0100

    Initial iOS project commit

---
+++ ios/SideProject/SideProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SideProject
+//
+//  Created by Gordon Krull on 2017-09-28.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ ios/SideProject/SideProjectTests/ViewControllerSpec.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerSpec.swift
+//  SideProjectTests
+//
+//  Created by Gordon Krull on 2017-09-28.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+import Nimble
+import Quick
+
+class ViewControllerSpec: QuickSpec {
+    
+}
