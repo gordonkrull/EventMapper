0f8db4436c3f46f97e71b1f3ee9a7a010282cfc4
Gordon Krull <gordonekru@gmail.com>
2017-10-17 13:53:13 -0400

    Add CoreLocation and MapView to main view controller

--- ios/SideProject/SideProject/ViewController.swift
+++ ios/SideProject/SideProject/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.startStandardUpdates()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func startStandardUpdates() {
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager?.delegate = self
+            locationManager?.desiredAccuracy = kCLLocationAccuracyKilometer
+            
+            locationManager?.distanceFilter = 500
+            locationManager?.startUpdatingLocation()
+        }
     }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("HeLLO")
+    }
 }
 
