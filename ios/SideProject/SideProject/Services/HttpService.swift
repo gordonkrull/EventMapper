4fa24e6977d459ea7f1a1e8b8f482b30a19f6a46
Gordon Krull <gordonekru@gmail.com>
2017-10-23 17:44:14 -0400

    Add HttpService skeleton. This will back the EventService eventually.

---
+++ ios/SideProject/SideProject/Services/HttpService.swift
@@ -0,0 +1,17 @@
+//
+//  HttpService.swift
+//  SideProject
+//
+//  Created by Gordon Krull on 2017-10-23.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+import Foundation
+
+protocol HttpService {
+    
+}
+
+class RealHttpService: HttpService {
+    
+}
