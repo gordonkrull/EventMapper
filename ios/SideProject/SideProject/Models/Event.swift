7c6f8bbffcffc1869b6a479899f6d4c8f581928a
Gordon Krull <gordonekru@gmail.com>
2017-10-23 17:43:38 -0400

    Make Events equatable

--- ios/SideProject/SideProject/Models/Event.swift
+++ ios/SideProject/SideProject/Models/Event.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import CoreLocation
+
+struct Event {
+    var coordinate: CLLocationCoordinate2D
+    var title: String
+    var subtitle: String
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
+
+extension Event: Equatable {
+    public static func ==(lhs: Event, rhs: Event) -> Bool {
+        return lhs.coordinate.latitude == rhs.coordinate.latitude &&
+            lhs.coordinate.longitude == rhs.coordinate.longitude &&
+            lhs.title == rhs.title &&
+            lhs.subtitle == rhs.subtitle
+    }
+}
+
