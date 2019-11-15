0999a4af6305d98105a6885dea90d6c9490763f3
Gordon Krull <gordonekru@gmail.com>
2019-11-15 16:22:43 +0800

    Configure backend for testing and add first Events Controller unit test

---
+++ backend/EventMapper/src/test/kotlin/org/gordonkrull/eventmapper/controllers/EventsControllerTest.kt
@@ -0,0 +1,26 @@
+package org.gordonkrull.eventmapper.controllers
+
+import org.assertj.core.api.Assertions.assertThat
+import org.gordonkrull.eventmapper.models.Coordinate
+import org.gordonkrull.eventmapper.models.Event
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class EventsControllerTest {
+
+    @InjectMocks
+    lateinit var eventsController: EventsController
+
+    @Test
+    fun getEvents_returnsStubbedTestEvent() {
+        val testEvent = Event(
+                Coordinate(37.7577627, -122.4726194),
+                "Test Event",
+                "Test Subtitle"
+        )
+        assertThat(eventsController.getEvents()).isEqualTo(testEvent)
+    }
+}
