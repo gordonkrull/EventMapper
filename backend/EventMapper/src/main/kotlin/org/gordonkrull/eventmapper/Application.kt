b200c51fff317cbe13544d6877061cf37f4b35b6
Gordon Krull <gordonekru@gmail.com>
2019-11-15 17:19:39 +0800

    Update Application to run with less annotations and newer syntax
    - add ContextLoads unit test to ensure app can launch

--- backend/EventMapper/src/main/kotlin/org/gordonkrull/eventmapper/Application.kt
+++ backend/EventMapper/src/main/kotlin/org/gordonkrull/eventmapper/Application.kt
@@ -1,13 +1,11 @@
 package org.gordonkrull.eventmapper
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Configuration
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@Configuration
 class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    runApplication<Application>(*args)
 }
