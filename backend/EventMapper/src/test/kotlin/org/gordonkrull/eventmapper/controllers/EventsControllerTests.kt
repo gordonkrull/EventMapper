f630d0269760986650234cceb81402777a32fd7d
Gordon Krull <gordonekru@gmail.com>
2019-11-15 17:20:01 +0800

    Pluralize test files

--- backend/EventMapper/src/test/kotlin/org/gordonkrull/eventmapper/controllers/EventsControllerTests.kt
+++ backend/EventMapper/src/test/kotlin/org/gordonkrull/eventmapper/controllers/EventsControllerTests.kt
@@ -9,7 +9,7 @@ import org.mockito.InjectMocks
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
-class EventsControllerTest {
+class EventsControllerTests {
 
     @InjectMocks
     lateinit var eventsController: EventsController
